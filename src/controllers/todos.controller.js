f6da9c327b1c0d2d06410fd5ef38bad233eb1fc2
Sudip Aryal <sudip.aryal@nextiva.com>
2022-06-26 19:44:14 -0500

    updated delete todo code

--- src/controllers/todos.controller.js
+++ src/controllers/todos.controller.js
@@ -45,7 +45,11 @@ const updateTodos = async (req, res) => {
 
 const deleteTodos = async (req, res) => {
   try {
-    await Todo.findByIdAndDelete(req.params.id);
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
+      res.status(400).json({ message: "Todo Id does not exist" });
+    }
+    await todo.remove();
     res.status(200).json({ message: `Todo with id: ${req.params.id} deleted` });
   } catch (error) {
     req.status(404).json({ message: "Cannot delete todos" });
