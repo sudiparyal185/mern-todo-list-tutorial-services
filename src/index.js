c7fd8e010e546efe7d1330c876f35884485672aa
Sudip Aryal <sudip.aryal@nextiva.com>
2022-07-10 17:26:18 -0500

    fixed mongo error

--- src/index.js
+++ src/index.js
@@ -5,16 +5,31 @@ const todosRoutes = require("./routes/todos.routes");
 const userRoutes = require("./routes/users.routes");
 const connectDatabase = require("./database/db");
 
-const PORT = process.env.PORT || 3005;
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+if (cluster.isMaster) {
+  console.log(`Number of CPUs are ${totalCpus}`);
+  console.log(`Master ${process.pid} is running`);
 
-app.use("/rest/todos", todosRoutes);
-app.use("/rest/users", userRoutes);
+  // Create worker threads
+  for (let i = 0; i < totalCpus; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    console.log(`Let fork another worker`);
+    cluster.fork();
+  });
+} else {
+  const PORT = process.env.PORT || 3005;
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
 
-app.listen(PORT, () => {
-  connectDatabase();
-  console.log(`Server listening on port ${PORT}`);
-});
+  app.use("/rest/todos", todosRoutes);
+  app.use("/rest/users", userRoutes);
+
+  app.listen(PORT, () => {
+    connectDatabase();
+    console.log(`Server listening on port ${PORT}`);
+  });
+}
