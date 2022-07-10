ff511c7f7845b775ca672852c71a786fe6c46f87
Sudip Aryal <sudip.aryal@nextiva.com>
2022-07-10 17:17:43 -0500

    attempt to fix mongoose error

--- src/index.js
+++ src/index.js
@@ -1,37 +1,20 @@
 const express = require("express");
-const cluster = require("cluster");
 const cors = require("cors");
-const totalCpus = require("os").cpus().length;
 require("dotenv").config();
 const todosRoutes = require("./routes/todos.routes");
 const userRoutes = require("./routes/users.routes");
 const connectDatabase = require("./database/db");
 
-if (cluster.isMaster) {
-  console.log(`Number of CPUs are ${totalCpus}`);
-  console.log(`Master ${process.pid} is running`);
+const PORT = process.env.PORT || 3005;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-  // Create worker threads
-  for (let i = 0; i < totalCpus; i++) {
-    cluster.fork();
-  }
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
-    console.log(`Let fork another worker`);
-    cluster.fork();
-  });
-} else {
-  const PORT = process.env.PORT || 3005;
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+app.use("/rest/todos", todosRoutes);
+app.use("/rest/users", userRoutes);
 
-  app.use("/rest/todos", todosRoutes);
-  app.use("/rest/users", userRoutes);
-
-  app.listen(PORT, () => {
-    connectDatabase();
-    console.log(`Server listening on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  connectDatabase();
+  console.log(`Server listening on port ${PORT}`);
+});
